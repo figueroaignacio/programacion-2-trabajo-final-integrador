67c26c43e02ab7ad315bb2dfa29bb679075fdc58
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-11-03 18:05:37 -0300

    remover comentarios

--- src/programacion/pkg2/trabajo/pkgfinal/integrador/Programacion2TrabajoFinalIntegrador.java
+++ src/programacion/pkg2/trabajo/pkgfinal/integrador/Programacion2TrabajoFinalIntegrador.java
@@ -3,7 +3,7 @@ package programacion.pkg2.trabajo.pkgfinal.integrador;
 public class Programacion2TrabajoFinalIntegrador {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Que onda muchachos");
     }
     
 }
