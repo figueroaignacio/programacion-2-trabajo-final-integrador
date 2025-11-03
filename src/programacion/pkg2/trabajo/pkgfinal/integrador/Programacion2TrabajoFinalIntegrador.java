f29cf857f62f0e6d6eaf160a16c1cb40a3a5feae
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-11-03 17:56:20 -0300

    inicializar proyecto

---
+++ src/programacion/pkg2/trabajo/pkgfinal/integrador/Programacion2TrabajoFinalIntegrador.java
@@ -0,0 +1,9 @@
+package programacion.pkg2.trabajo.pkgfinal.integrador;
+
+public class Programacion2TrabajoFinalIntegrador {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
